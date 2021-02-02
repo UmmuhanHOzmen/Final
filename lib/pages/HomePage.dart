f99aa6b11b862e7019f75e7e393208c4904fbf36
UmmuhanHOzmen <ummuhanifeozmen@gmail.com>
2021-02-02 14:38:06 +0300

    temel düzenlemeler

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page Başlık'),
+        centerTitle: true,
         leading: GestureDetector(
           onTap: () {
             print('menuye tıklandı');
@@ -15,7 +16,35 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(),
+      body: Center(
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://www.bbc.co.uk/news/special/2015/newsspec_10857/bbc_news_logo.png?cb=1'),
+                    ListTile(
+                      leading: Icon(Icons.arrow_drop_down_circle),
+                      title: Text('Haber başlığı'),
+                      subtitle: Text('Yazar'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text('Açıklama kısmı buraya gelecek'),
+                    ),
+                    ButtonBar(
+                      alignment: MainAxisAlignment.start,
+                      children: [
+                        FlatButton(onPressed: null, child: Text('Habere Git'))
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }),
+      ),
     );
   }
 }
